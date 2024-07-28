dec603bdc55fe5348af49566a9ddc94bf67aa0bc
Tran Minh Thanh <trmthanh220895@gmail.com>
2024-07-28 21:35:50 +0800

    edite

--- ui_react/src/Pages/HomePage.jsx
+++ ui_react/src/Pages/HomePage.jsx
@@ -3,7 +3,7 @@ function HomePage() {
 
     return (
       <div>
-      Home Page was created by Tran Minh Thanh at 06:00  ddd8888
+      Home Page was created by Tran Minh Thanh at 06:00  ddd nâng cấp
       </div>
     )
   }
