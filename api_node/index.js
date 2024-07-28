a98d08f1467359e126a6d6fbba5f59b276c4893d
Tran Minh Thanh <trmthanh220895@gmail.com>
2024-07-28 18:18:57 +0800

    iooo

---
+++ api_node/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World Dan Chi Sua Qua')
+})
+
+app.get('/sua', (req, res) => {
+  res.send('sua')
+})
+
+app.get('/about', (req, res) => {
+  res.send('about')
+})
+
+app.listen(5000, () => {
+  console.log(`Example app listening on port 5000`)
+})
