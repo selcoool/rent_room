7ed3fdaa90b221090d316c28e1a04e2f5d3b5fa0
Tran Minh Thanh <trmthanh220895@gmail.com>
2024-07-31 02:30:18 +0800

    p

--- api_node/index.js
+++ api_node/index.js
@@ -13,6 +13,6 @@ app.get('/about', (req, res) => {
   res.send('about')
 })
 
-app.listen(6000, () => {
-  console.log(`Example app listening on port 6000`)
+app.listen(4200, () => {
+  console.log(`Example app listening on port 4200`)
 })
