866b9a1ef5ea7345a7ea2a3b50f02e2c154074d9
Tran Minh Thanh <trmthanh220895@gmail.com>
2024-08-02 17:18:37 +0800

    mysql

--- api_node/index.js
+++ api_node/index.js
@@ -1,5 +1,42 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const { PrismaClient } = require('@prisma/client');
+// import { PrismaClient } from '@prisma/client';
+
+var  corsOptions  = {
+  origin:["http://localhost:4000"], //frontend url
+  credentials: true,
+  allowedHeaders:'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  methods:'GET,HEAD,PUT,PATCH,POST,DELETE',
+  exposedHeaders:'Content-Range, X-Content-Range'
+}
+
+
+app.use(cors(corsOptions));
+
+app.use(express.json({ limit: '30mb' }));
+app.use(express.urlencoded({ extended: true, limit: '30mb' }));
+
+
+const prisma = new PrismaClient();
+
+app.get('/test', async(req, res) => {
+
+  //  const inf= await prisma.hinh_anh.findMany({
+  //   where:{
+  //     hinh_id:1
+  //   }
+  //  });
+
+   const inf= await prisma.hinh_anh.findMany();
+//     prisma.hinh_anh.findMany();
+// //   res.send('Hello World!')
+// console.log('sssssssss',inf);
+// return inf;
+return res.status(200).json(inf)
+})
+
 
 app.get('/', (req, res) => {
   res.send('BACKEND CAP NHAT MOI UPDATED')
